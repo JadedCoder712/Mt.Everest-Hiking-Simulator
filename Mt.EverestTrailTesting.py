b28003e4968576d217a02007c430b405a0ec5333
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-29 19:36:50 -0400

    Updated from Brython Server: 3/29/2017 7:36:49 PM

--- Mt.EverestTrailTesting.py
+++ Mt.EverestTrailTesting.py
@@ -1,1 +1,11 @@
 gamemenu=input("Welcome to Kathmandu! You may: Press 1 to Start the Hike, press 2 to learn more about the trek up Mt. Everest, press 3 to see the map of the Mt. Everest trek, or press 4 to quit the program.")
+
+while gamemenu > 0:
+    if gamemenu is "2":
+        
+    if gamemenu is "3":
+        
+    if gamemenu is "4":
+        
+    if gamemenu is "1":
+        
