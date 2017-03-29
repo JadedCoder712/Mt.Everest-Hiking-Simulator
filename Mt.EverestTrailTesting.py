b4644d61c20d4f91663f9539bdd1ee59803cab4b
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-29 19:29:23 -0400

    Update Mt.EverestTrailTesting.py

--- Mt.EverestTrailTesting.py
+++ Mt.EverestTrailTesting.py
@@ -1,6 +1,1 @@
-"""
-There is a time limit to get to the top. The time limit goes down by the amount of options you choose. 
-The place you choose to start affects your time limit but also affects the difficulty.
-The difficulty affects how much gear you lose and how often you lose it.
-Your backpack is what carries all your gear. If you lose your backpack, you will lose almost all of your gear/items.
-The choices you make will affect your time limit, the 
+
