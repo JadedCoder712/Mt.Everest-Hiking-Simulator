1b36038db5c5a5b4ab606da6bdf174274f68abe8
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-29 19:50:46 -0400

    Updated from Brython Server: 3/29/2017 7:50:45 PM

--- Mt.EverestTrailTesting.py
+++ Mt.EverestTrailTesting.py
@@ -1,11 +1,11 @@
-gamemenu=input("Welcome to Kathmandu! You may: Press 1 to Start the Hike, press 2 to learn more about the trek up Mt. Everest, press 3 to see the map of the Mt. Everest trek, or press 4 to quit the program.")
-
-while gamemenu > 0:
+truthholder=True
+while truthholder is True:
+    gamemenu=input("Welcome to Kathmandu! You may: Press 1 to Start the Hike, press 2 to learn more about the trek up Mt. Everest, press 3 to see the map of the Mt. Everest trek, or press 4 to quit the program.")
     if gamemenu is "2":
-        input("The hike up Mount Everest is a long and difficult journey. It is filled with Hazards and danger.")
+        input("The hike up Mount Everest is a long and difficult journey. It is filled with Hazards and danger. Type OK to continue.")
     if gamemenu is "3":
-        input("This part of the Mount Everest Trail is under contruction right now.")
+        input("This part of the Mount Everest Trail is under contruction right now. Press Enter to continue.")
     if gamemenu is "4":
-        
+        truthholder=False
     if gamemenu is "1":
-        
+        print("Bye")
