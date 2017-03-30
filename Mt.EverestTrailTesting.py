d033679b15f2bf8f6a4ddd094357a5954633aa89
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-30 11:27:21 -0400

    Updated from Brython Server: 3/30/2017 11:27:20 AM

--- Mt.EverestTrailTesting.py
+++ Mt.EverestTrailTesting.py
@@ -43,4 +43,4 @@ if store is "1":
     while truthholder is False:
         itemmenu=input("You have {0} rations, {1} sets of clothes, {2} ice picks, {3} tents, {4} canisters of fuel, and {5} liters of water. 1. Go to store, 2. Learn more about each item.".format(rations, clothes, icepicks, tents, fuel, water)) 
         if itemmenu is "2":
-            input("Rations are what keep you alive. Without rations, your health will get worse. Clothes are what keep you warm. If they 
+            input("Rations are what keep you alive. Without rations, your health will get worse. Clothes are what keep you warm. If they get wet, you will need a new set of clothes. If you do not change clothes or dry your clothes, you will get cold and your health will get worse. Ice picks help you scale ice walls. Ice picks break, so you will need to make sure to have some at all times. Tents are what you sleep in. These can break, and if 
