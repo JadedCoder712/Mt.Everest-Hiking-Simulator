8c9265141fab76b006f300997c084f21d4057cd2
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-29 20:08:48 -0400

    Updated from Brython Server: 3/29/2017 8:08:46 PM

--- Mt.EverestTrailTesting.py
+++ Mt.EverestTrailTesting.py
@@ -1,3 +1,4 @@
+import sys
 truthholder=True
 while truthholder is True:
     gamemenu=input("Welcome to Kathmandu! You may: Press 1 to Start the Hike, press 2 to learn more about the trek up Mt. Everest, press 3 to see the map of the Mt. Everest trek, or press 4 to quit the program.")
@@ -6,6 +7,19 @@ while truthholder is True:
     if gamemenu is "3":
         input("This part of the Mount Everest Trail is under contruction right now. Press Enter to continue.")
     if gamemenu is "4":
-        truthholder=False
+        sys.exit(0)
     if gamemenu is "1":
-        print("Bye")
+        truthholder=False
+while truthholder is False:
+    playertype=input("Many people attempt to hike Everest each year. Who are you? You may be: 1. A CEO from America, 2. An accountant from England, 3. A scientist from Australia. Press 4 to learn more about these choices and how they affect the game.")
+    if playertype is "4":
+        input("The type of person you are affects how much money you have. For instance, a CEO will have more money than an accountant or scientist. However, if you choose the CEO, this will reduce your score at the end of the game. Press Enter to continue.")
+    if playertype is "1":
+        money=10,000
+        truthholder=True
+    if playertype is "2":
+        money=7,000
+        truthholder=True
+    if playertype is "3":
+        money=6,500
+        truthholder=True
