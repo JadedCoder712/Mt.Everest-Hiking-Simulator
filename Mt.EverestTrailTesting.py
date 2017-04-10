f9ee0506ee128bb9c06e6077edd59a0ad0d84df2
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-04-10 14:09:14 -0400

    Updated from Brython Server: 4/10/2017 2:09:12 PM

--- Mt.EverestTrailTesting.py
+++ Mt.EverestTrailTesting.py
@@ -14,7 +14,7 @@ the_hike=1
 while truth_holder is True:
     game_menu=input("Welcome to Kathmandu! You may: Press 1 to Start the Hike, press 2 to learn more about the trek up Mt. Everest, press 3 to see the map of the Mt. Everest trek, or press 4 to quit the program.")
     if game_menu is "2":
-        input("The hike up Mount Everest is a long and difficult journey. It is filled with Hazards and danger. Type OK to continue.")
+        input("The hike up Mount Everest is a long and difficult journey. It is filled with Hazards and danger. Type OK to continue.")  #Need to make more lengthy
     if game_menu is "3":
         input("This part of the Mount Everest Trail is under contruction right now. Press Enter to continue.")
     if game_menu is "4":
